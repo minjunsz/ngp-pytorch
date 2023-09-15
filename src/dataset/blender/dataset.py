fix numerical unstability

src/dataset/blender/dataset.py
@@ -43,12 +43,12 @@ class BlenderDataset(Dataset):
             imgs.append(imageio.imread(fname))
             poses.append(np.array(frame["transform_matrix"]))
         # keep all 4 channels (RGBA)
-        self.imgs = np.array(imgs, dtype=np.float32) / 255.0
-        self.poses = np.array(poses, dtype=np.float32)
+        self.imgs = np.array(imgs, dtype=np.float64) / 255.0
+        self.poses = np.array(poses, dtype=np.float64)
 
         self.H, self.W = self.imgs[0].shape[:2]
         camera_angle_x = float(self.meta["camera_angle_x"])
-        focal = 0.5 * self.W / np.tan(0.5 * camera_angle_x)
+        self.focal = 0.5 * self.W / np.tan(0.5 * camera_angle_x)
 
         self.render_poses = torch.stack(
             [
@@ -61,7 +61,7 @@ class BlenderDataset(Dataset):
         if half_res:
             self.H = self.H // 2
             self.W = self.W // 2
-            self.focal = focal / 2.0
+            self.focal = self.focal / 2.0
 
             imgs_half_res = np.zeros((self.imgs.shape[0], self.H, self.W, 4))
             for i, img in enumerate(self.imgs):
@@ -71,7 +71,9 @@ class BlenderDataset(Dataset):
             self.imgs = imgs_half_res
 
         # White Background
-        self.imgs = self.imgs[..., :3] * self.imgs[..., -1:] + (1 - self.imgs[..., -1:])
+        self.imgs = self.imgs[..., :3] * self.imgs[..., -1:] + (
+            1.0 - self.imgs[..., -1:]
+        )
 
     @property
     def K(self):

src/run_nerf_helpers.py
@@ -48,42 +48,26 @@ class NeRFSmall(nn.Module):
         self.num_layers = num_layers
         self.hidden_dim = hidden_dim
         self.geo_feat_dim = geo_feat_dim
-
-        sigma_net = []
-        for l in range(num_layers):
-            if l == 0:
-                in_dim = self.input_ch
-            else:
-                in_dim = hidden_dim
-
-            if l == num_layers - 1:
-                out_dim = 1 + self.geo_feat_dim  # 1 sigma + 15 SH features for color
-            else:
-                out_dim = hidden_dim
-
-            sigma_net.append(nn.Linear(in_dim, out_dim, bias=False))
-
-        self.sigma_net = nn.ModuleList(sigma_net)
+        self.sigma_net = nn.Sequential(
+            nn.Linear(self.input_ch, hidden_dim, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, hidden_dim, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, 1 + self.geo_feat_dim, bias=False),
+        )
 
         # color network
         self.num_layers_color = num_layers_color
         self.hidden_dim_color = hidden_dim_color
-
-        color_net = []
-        for l in range(num_layers_color):
-            if l == 0:
-                in_dim = self.input_ch_views + self.geo_feat_dim
-            else:
-                in_dim = hidden_dim
-
-            if l == num_layers_color - 1:
-                out_dim = 3  # 3 rgb
-            else:
-                out_dim = hidden_dim
-
-            color_net.append(nn.Linear(in_dim, out_dim, bias=False))
-
-        self.color_net = nn.ModuleList(color_net)
+        self.color_net = nn.Sequential(
+            nn.Linear(self.input_ch_views + self.geo_feat_dim, hidden_dim, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, hidden_dim, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, hidden_dim, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, 3, bias=False),
+        )
 
     def forward(self, x):
         input_pts, input_views = torch.split(
@@ -92,19 +76,13 @@ class NeRFSmall(nn.Module):
 
         # sigma
         h = input_pts
-        for l in range(self.num_layers):
-            h = self.sigma_net[l](h)
-            if l != self.num_layers - 1:
-                h = F.relu(h, inplace=True)
+        h = self.sigma_net(h)
 
         sigma, geo_feat = h[..., 0], h[..., 1:]
 
         # color
         h = torch.cat([input_views, geo_feat], dim=-1)
-        for l in range(self.num_layers_color):
-            h = self.color_net[l](h)
-            if l != self.num_layers_color - 1:
-                h = F.relu(h, inplace=True)
+        h = self.color_net(h)
 
         # color = torch.sigmoid(h)
         color = h